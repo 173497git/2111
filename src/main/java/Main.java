093acf8566f5572c5c7482e083063dfc206d0081
173497git <161622376+173497git@users.noreply.github.com>
2024-03-13 15:52:50 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String word = "hello world";
+        for (int i = 0; i < word.length(); i++) {
+            System.out.println(word.charAt(i));
+        }
+    }
+}
